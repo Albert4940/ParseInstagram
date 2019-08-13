dba0ab4ffe9011163dc7ec92a3e053d77dc6fc5a
Albert-Ubun <dorcealbertmary@gmail.com>
2019-08-13 02:19:15 -0400

    I add a savePost for post

--- app/src/main/java/com/albert/parseinstagram/MainActivity.java
+++ app/src/main/java/com/albert/parseinstagram/MainActivity.java
@@ -3,6 +3,7 @@ package com.albert.parseinstagram;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -10,6 +11,8 @@ import android.widget.ImageView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -30,7 +33,35 @@ public class MainActivity extends AppCompatActivity {
         ivPoster = findViewById(R.id.ivPosterImage);
         btnSubmit = findViewById(R.id.btnSubmit);
 
-        queryPosts();
+        //queryPosts();
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               String description = etDescription.getText().toString();
+                ParseUser user = ParseUser.getCurrentUser();
+                savePost(description,user);
+            }
+        });
+    }
+
+    private void savePost(String description, ParseUser parseUser) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setUser(parseUser);
+        //post.setImage();
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null)
+                {
+                    Log.d(TAG,"error while saving ");
+                    e.printStackTrace();
+                    return;
+                }
+                Log.d(TAG,"success ! ");
+                etDescription.setText("");
+            }
+        });
     }
 
     private void queryPosts(){
